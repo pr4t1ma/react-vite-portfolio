1f6c3d917e8140a543f6079aa70e25fc93b463c8
Pam <prati.ma@hotmail.com>
2024-11-24 17:01:47 +0100

    Remove date from the sPanelsdata

--- src/components/TabbedPanels/TabbedPanelsData.ts
+++ src/components/TabbedPanels/TabbedPanelsData.ts
@@ -8,29 +8,29 @@ export const items = [
 
 export const panelsData = [
   {
-    heading: "HTML  ",
-    date: "2014 ",
+    heading: "HTM  ",
+    // date: "2014 ",
     text: `During my self-learning journey with HTML, I became fascinated by how important it is in web development. With regular practice and exploration, I quickly grew from a beginner to a confident HTML developer. 
 `
   },
   {
     heading: "CSS",   
-    date: "2014",
+    // date: "2014",
     text: "During my internship, I learned CSS by translating design mockups into nice layouts. I used CSS to add margins, padding for spacing, background colors, images, and card designs. CSS helped me make the designs look polished and professional.",
   },
   {
     heading: "Javascript ",
-    date: "2023- 2024",
-    text: `"Learn to use variables, console.log, types, operators, functions, objects, arrays, JSON, DOM manipulation, and APIs. Gain confidence to build websites effectively. Gain foundational knowledge and practical skills for web development."`,
+    // date: "2023- 2024",
+    text: `Learn to use variables, console.log, types, operators, functions, objects, arrays, JSON, DOM manipulation, and APIs. Gain confidence to build websites effectively. Gain foundational knowledge and practical skills for web development.`,
   },
   {
     heading: "React",
-    date: "2024",
+    // date: "2024",
     text: "During my course, I delved deep into React to complete the project. I utilized React with Tailwind CSS and also learned React Native with Expo to develop a mobile app.During my course I have lern to work with components difference between higher component and dummy component.",
   },
   {
     heading: "UI/UX",
-    date: "2022-2024",
+    // date: "2022-2024",
     text: `I completed a UI/UX course at Redi School, 
     where I finished both basic and intermediate courses along with projects.
      Additionally, I enrolled in a UX course at Techlabs and completed a project.
