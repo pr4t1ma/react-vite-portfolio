fbb68bfbb56f4487af60da0dbe330067cf747941
Pam <pratymaa@gmail.com>
2026-04-15 18:03:27 +0200

    remove background

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,16 +1,35 @@
 import { FC } from "react";
+import { AnimatedHeading } from "../AnimatedHeading/AnimatedHeading";
 
 type HeadingProps = {
   text: string;
+  variant?: "fade-scale" | "slide" | "rotate";
+  withUnderline?: boolean;
+  withBorders?: boolean;
 };
-export const Heading: FC<HeadingProps> = ({ text }) => {
+
+export const Heading: FC<HeadingProps> = ({
+  text,
+  variant = "fade-scale",
+  withUnderline = false,
+  withBorders = false,
+}) => {
   return (
-    <div className="w-2/4 mx-auto items-left flex gap-5 justify-left my-20">
-      <div className="border border-1 border-solid border-gray dark:border-gray-600 opacity-50 dark:opacity-30 h-0 flex-1 my-4"></div>
-      <h1 className="text-3xl font-poppins font-bold whitespace-nowrap animate-shimmer text-gray-800 dark:text-white">
+    <div className="w-full md:w-3/4 mx-auto flex items-center justify-center gap-6 my-20 px-4">
+      {withBorders && (
+        <div className="h-0.5 flex-1 bg-gradient-to-r from-transparent to-orange-600 dark:to-orange-400"></div>
+      )}
+      <AnimatedHeading
+        level={2}
+        className="text-4xl lg:text-5xl font-poppins font-bold whitespace-nowrap text-gray-800 dark:text-white"
+        variant={variant}
+        withUnderline={withUnderline}
+      >
         {text}
-      </h1>
-      <div className="border border-1 border-solid border-gray dark:border-gray-600 opacity-50 dark:opacity-30 h-0 flex-1 my-4"></div>
+      </AnimatedHeading>
+      {withBorders && (
+        <div className="h-0.5 flex-1 bg-gradient-to-l from-transparent to-orange-600 dark:to-orange-400"></div>
+      )}
     </div>
   );
 };
